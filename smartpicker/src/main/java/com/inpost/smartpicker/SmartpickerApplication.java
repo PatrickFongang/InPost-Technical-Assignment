67f0260320966192038df32527f3fb6b5ea32799
LegalnyPoeta <patrickfongang.contact@gmail.com>
2026-04-30 18:43:52 +0200

    initialize Spring Boot project with Web and Lombok dependencies

---
+++ smartpicker/src/main/java/com/inpost/smartpicker/SmartpickerApplication.java
@@ -0,0 +1,13 @@
+package com.inpost.smartpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartpickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartpickerApplication.class, args);
+	}
+
+}
---
+++ smartpicker/src/test/java/com/inpost/smartpicker/SmartpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inpost.smartpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartpickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
