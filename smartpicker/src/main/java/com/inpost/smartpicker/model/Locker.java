da8ea6e641aa445a948aeffd6dd97649b3450e8c
LegalnyPoeta <patrickfongang.contact@gmail.com>
2026-04-30 19:40:19 +0200

    add data models and InPost API integration service

---
+++ smartpicker/src/main/java/com/inpost/smartpicker/model/Locker.java
@@ -0,0 +1,29 @@
+﻿package com.inpost.smartpicker.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Locker {
+    private String name;
+
+    @JsonProperty("location_type")
+    private String locationType;
+
+    @JsonProperty("easy_access_zone")
+    private boolean easyAccessZone;
+
+    @JsonProperty("recommended_low_interest_box_machines_list")
+    private List<String> recommendedLowInterestBoxMachinesList;
+
+    private Location location;
+
+    @JsonProperty("address_details")
+    private AddressDetails addressDetails;
+
+
+}
---
+++ smartpicker/src/main/java/com/inpost/smartpicker/service/InPostService.java
@@ -0,0 +1,35 @@
+﻿package com.inpost.smartpicker.service;
+
+import com.inpost.smartpicker.model.InPostResponse;
+import com.inpost.smartpicker.model.Locker;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class InPostService {
+    private final RestTemplate restTemplate;
+
+    public InPostService() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public List<Locker> fetchLockersByCity(String city) {
+        String url = "https://api-global-points.easypack24.net/v1/points?city=" + city;
+
+        try {
+            InPostResponse response = restTemplate.getForObject(url, InPostResponse.class);
+
+            if (response != null && response.getItems() != null) {
+                return response.getItems();
+            }
+        } catch (Exception e) {
+            System.err.println("Błąd pobierania danych z InPost API: " + e.getMessage());
+        }
+
+        return Collections.emptyList();
+    }
+
+}
